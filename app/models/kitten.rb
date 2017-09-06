5203bfa5ac29de3dc646cce949910a8c82e9cadc
Salomanuel <salomanuel.aux+code@gmail.com>
2017-09-06 17:02:25 +0200

    add validations

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,7 @@
 class Kitten < ApplicationRecord
+	# validates: name, {max_lenght: [20]}
+	validates :name, 			presence: true, length: { maximum: 20 }
+	validates :age,				presence: true
+	validates :cuteness, 	presence: true
+	validates :softness, 	presence: true
 end
