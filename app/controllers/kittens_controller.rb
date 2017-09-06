5f8c4bf01dd585cccd21c239b5026c22ce4aed7b
Salomanuel <salomanuel.aux+code@gmail.com>
2017-09-06 12:39:00 +0200

    implement #new and #create Kitten

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -7,4 +7,24 @@ class KittensController < ApplicationController
 	def show
 		@kitten  = Kitten.find(params[:id])
 	end
+
+	def new
+		@kitten  = Kitten.new
+		@scale   = (1..10).to_a.map { |i| [i, i] }
+	end
+
+	def create
+		@kitten  = Kitten.new(kitten_params)
+		if @kitten.save
+			# render inline: "<h1>ok</h1>"
+			redirect_to @kitten
+		else
+			render inline: "<h1>oh no</h1>"
+		end
+	end
+
+	private
+		def kitten_params
+			params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+		end
 end
