cba7cf95ddfe70d22b8de17b0b57e1f7514a740b
Salomanuel <salomanuel.aux+code@gmail.com>
2017-09-07 17:38:14 +0200

    form can take a User name

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,8 @@
 class StaticPagesController < ApplicationController
+	def form
+		if params[:name]
+			@user   = params[:name]
+			@photos = flickr.photos.search(:user_id => 'Salomanuel')
+		end
+	end
 end
